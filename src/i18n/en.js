7bbfb4c51f45cb4d1c8e06d23b9657904a737a1f
a.rylkov <larcanus@yandex.ru>
2025-10-25 18:28:10 +0300

    add local

---
+++ src/i18n/en.js
@@ -0,0 +1,21 @@
+export default {
+  nav: {
+    main: 'Main',
+    download: 'Download',
+    faq: 'FAQ',
+    lang: 'RU',
+  },
+  main: {
+    title: 'Welcome to the Chrome Extension Support Site',
+    description: 'Preview, guide, and updates for our Chrome extension.',
+    updates: 'Updates',
+  },
+  download: {
+    title: 'Download',
+    description: 'Get the custom extension and supporting software here.',
+  },
+  faq: {
+    title: 'FAQ',
+    description: 'Frequently Asked Questions',
+  },
+}
--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,55 +1,17 @@
 import { createI18n } from 'vue-i18n'
+import en from './en'
+import ru from './ru'
 
 const messages = {
-  en: {
-    nav: {
-      main: 'Main',
-      download: 'Download',
-      faq: 'FAQ',
-      lang: 'RU',
-    },
-    main: {
-      title: 'Welcome to the Chrome Extension Support Site',
-      description: 'Preview, guide, and updates for our Chrome extension.',
-      updates: 'Updates',
-    },
-    download: {
-      title: 'Download',
-      description: 'Get the custom extension and supporting software here.',
-    },
-    faq: {
-      title: 'FAQ',
-      description: 'Frequently Asked Questions',
-    },
-  },
-  ru: {
-    nav: {
-      main: 'Главная',
-      download: 'Скачать',
-      faq: 'FAQ',
-      lang: 'EN',
-    },
-    main: {
-      title: 'Добро пожаловать на сайт поддержки расширения Chrome',
-      description: 'Превью, руководство и обновления для нашего расширения.',
-      updates: 'Обновления',
-    },
-    download: {
-      title: 'Скачать',
-      description: 'Здесь вы можете получить расширение и вспомогательное ПО.',
-    },
-    faq: {
-      title: 'FAQ',
-      description: 'Часто задаваемые вопросы',
-    },
-  },
+	en,
+	ru,
 }
 
 const i18n = createI18n({
-  legacy: false,
-  locale: 'ru',
-  fallbackLocale: 'en',
-  messages,
+	legacy: false,
+	locale: 'ru',
+	fallbackLocale: 'en',
+	messages,
 })
 
 export default i18n
